58a3de3cdc5986c07c36d6de0d9de2951570b2d7
baikoby228 <baikoby228@gmail.com>
2025-12-17 20:35:27 +0300

    Create parser.py

---
+++ parser.py
@@ -0,0 +1,31 @@
+import requests
+import fake_useragent
+from bs4 import BeautifulSoup
+
+URL = 'https://acmp.ru/index.asp'
+
+def get_tests(id_task: int) -> list:
+    user_agent = fake_useragent.UserAgent().random
+    header = {'user-agent': user_agent}
+    params = {
+        'main': 'task',
+        'id_task': id_task
+    }
+
+    response = requests.get(URL, params=params, headers=header)
+    soup = BeautifulSoup(response.text, "lxml")
+
+    table = soup.find('table', class_='main')
+    rows = table.find_all('tr', class_='white2')
+
+    res = []
+    for row in rows:
+        res.append(row.find_all('td')[1].text)
+
+    return res
+
+if __name__ == '__main__':
+    id_task = int(input())
+    tests = get_tests(id_task)
+    for test in tests:
+        print(test)
